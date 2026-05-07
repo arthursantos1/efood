4a1a783b74f7c1d4a1cbcb708d07aeec35ac55c0
arthur <arthur.s.vale@gmail.com>
2026-05-07 12:47:52 -0300

    Feito as configurações iniciais do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalCss } from './style'
+
+function App() {
+  return (
+    <div>
+      <>
+        <GlobalCss />
+        <h1>Teste</h1>
+      </>
+    </div>
+  )
+}
+
+export default App
---
+++ src/style.ts
@@ -0,0 +1,23 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+const cores = {
+  vermelho: '#e66767',
+  branco: '#fff',
+  douradoClaro: '#ffebd9',
+  brancoClaro: '#fff8f2'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Roboto, sans-serif;
+  list-style: none;
+}
+
+  body{
+    background-color: ${cores.brancoClaro};
+    color: ${cores.vermelho};
+}
+`
