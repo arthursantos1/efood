aeafee27fe127b0197473bc295464d82b6158183
arthur <arthur.s.vale@gmail.com>
2026-07-31 13:54:38 -0300

    removido o breakpoint no style

--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-import { breakpoints, cores } from '../../style'
+import { cores } from '../../style'
 
 export const Card = styled.div`
   position: relative;
